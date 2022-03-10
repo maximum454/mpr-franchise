4da1fe42a35b09df98b8e58f787f1faabbcf2b96
Maxim Karachyov <karachyov.m@gmail.com>
2022-03-10 15:46:33 +0300

    переверстка меню

--- build/js/mpr-franchise.js
+++ build/js/mpr-franchise.js
@@ -96,7 +96,7 @@ $(function () {
         let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
         let desktop = window.matchMedia('(min-width: 1024px)');
         let sc = $(this).attr("href");
-        let dn = $(sc).offset().top - 30;
+        let dn = $(sc).offset().top - 56;
 
         if (mobile.matches) {
             if ($(this).hasClass('nav__link--arrow')) {
@@ -133,70 +133,72 @@ $(function () {
         }, 800);
         return false;
     });
-})
 
-menu.addEventListener('click', (e) => {
-    const target = e.target;
-    menuToggle(target);
-})
+    const menu = document.getElementById('menu');
+    menu.addEventListener('click', (e) => {
+        const target = e.target;
+        menuToggle(target);
+    })
 
-function menuToggle(target) {
-    const nav = document.querySelector('.nav');
-    nav.classList.toggle('active');
-    target.classList.toggle('active');
-    document.querySelector('body').classList.toggle('lock');
-}
+    function menuToggle(target) {
+        const nav = document.querySelector('.nav');
+        nav.classList.toggle('active');
+        target.classList.toggle('active');
+        document.querySelector('body').classList.toggle('lock');
+    }
 
 
-const swiperReviews = new Swiper('.swiper-container-reviews', {
-    slidesPerView: 2,
-    loop: true,
-    autoHeight: true,
-    spaceBetween: 20,
-    watchOverflow: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    observer: true,
-    freeMode: true,
-    navigation: {
-        nextEl: '.swiper-reviews-next',
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
+    const swiperReviews = new Swiper('.swiper-container-reviews', {
+        slidesPerView: 2,
+        loop: true,
+        autoHeight: true,
+        spaceBetween: 20,
+        watchOverflow: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        observer: true,
+        freeMode: true,
+        navigation: {
+            nextEl: '.swiper-reviews-next',
         },
-        767: {
-            slidesPerView: 2,
-            spaceBetween: 20
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            }
         }
+    })
+
+    const navItems = document.querySelectorAll('.nav__item');
+
+    for (let navItem of navItems) {
+        navItem.addEventListener('click', function (e) {
+            this.classList.toggle('active');
+        })
+    }
+
+
+    const articlesItems = document.querySelectorAll('.articles__item');
+
+    for (let articlesItem of articlesItems) {
+        articlesItem.addEventListener('click', function (e) {
+            this.classList.toggle('active');
+        })
+    }
+
+
+    const dropDowns = document.querySelectorAll('.js-dropdown');
+
+    for (let dropDown of dropDowns) {
+        dropDown.addEventListener('click', function (e) {
+            this.classList.toggle('active');
+        })
     }
 })
 
-const navItems = document.querySelectorAll('.nav__item');
-
-for (let navItem of navItems) {
-    navItem.addEventListener('click', function (e) {
-        this.classList.toggle('active');
-    })
-}
-
-
-const articlesItems = document.querySelectorAll('.articles__item');
-
-for (let articlesItem of articlesItems) {
-    articlesItem.addEventListener('click', function (e) {
-        this.classList.toggle('active');
-    })
-}
-
-
-const dropDowns = document.querySelectorAll('.js-dropdown');
-
-for (let dropDown of dropDowns) {
-    dropDown.addEventListener('click', function (e) {
-        this.classList.toggle('active');
-    })
-}
 
 
 
