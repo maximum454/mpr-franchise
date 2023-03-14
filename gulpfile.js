01968f8ae820d2ffa63ac5779b6635b3b6866fd3
Maxim Karachyov <karachyov.m@gmail.com>
2023-03-14 08:38:11 +0300

    decor

--- gulpfile.js
+++ gulpfile.js
@@ -232,7 +232,7 @@ function watchFiles() {
     gulp.watch([path.watch.img], images);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 exports.fonts = fonts;
